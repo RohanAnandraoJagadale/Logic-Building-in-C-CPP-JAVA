e4b28e3522c72a916bd2cc096b5c4dafc5677a76
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-06 13:53:18 +0530

    Add files via upload

---
+++ Program101.c
@@ -0,0 +1,31 @@
+/*
+    Row = 4
+	Col = 4
+	
+	1 2 3 4
+	1 2 3 4 
+	1 2 3 4
+	1 2 3 4
+*/
+
+#include<stdio.h>
+
+void Display(int iRow, int iCol)
+{
+	
+}
+
+int main()
+{
+	intValue1 = 0, iValue2 = 0;
+	
+	printf("Enter number of Rows\n");
+	scanf("%d,&iValue1);
+	
+	printf("Enter number of Columns\n");
+	scanf("%d,&iValue2);
+	
+	Display(iValue1,iValue2);
+	
+	return 0;
+}
