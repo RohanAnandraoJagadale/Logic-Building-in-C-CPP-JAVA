bad54f7e168c4a041c4e0bf265b84c221b440d0b
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-06 15:11:37 +0530

    Add files via upload

---
+++ Program445.java
@@ -0,0 +1,29 @@
+// singly linear LL
+import java.util.*;
+
+class Node
+{
+	public int Data;
+	public Node Next;            // reference
+}
+
+class SinglyLL
+{
+	public Node Head;      // reference, composition
+	public int Count;
+	
+	public SinglyLL()
+	{
+		Head = null;
+		Count = 0;
+	}
+	
+}
+
+class Program445
+{
+	public static void main(String arg[])
+	{
+		SinglyLL obj = new SinglyLL();
+	}
+}
