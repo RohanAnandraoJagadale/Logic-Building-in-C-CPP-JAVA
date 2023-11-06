ad0f9d4ded11b05a523a3d5a5b930ee8b993a03a
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-06 15:03:39 +0530

    Add files via upload

---
+++ Program296.java
@@ -0,0 +1,34 @@
+// complete code
+import java.util.*;
+
+/*
+    Input : Hello
+	
+	H e l l o
+	H e l l o
+	H e l l o
+	H e l l o
+	H e l l o
+*/
+class Pattern
+{
+	public void Display(String str)
+	{
+		 
+	}
+}
+
+class Program296
+{
+	public static void main(String a[])
+	{
+		Pattern pobj = new Pattern();
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter sting : ");
+		String s = sobj.nextLine();
+		
+		pobj.Display(s);
+	}
+}
+
