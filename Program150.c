310526fd8a28c0c62fec73fdd9af9e059e8761d4
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-06 13:56:47 +0530

    Add files via upload

---
+++ Program150.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+char CharToggleX(char ch)
+{
+	if((ch >= 'A') && (ch <= 'Z'))
+	{
+		return ch + 32;  
+	}
+	else if((ch >= 'a') && (ch <= 'z'))
+	{
+		return ch - 32;
+	}
+	else
+	{
+		return ch;
+	}
+}
+
+int main()
+{
+	char cValue = '\0';
+	char cRet = '\0';
+	
+	printf("Enter The Character\n");
+	scanf("%c",&cValue);
+	
+	cRet = CharToggleX(cValue);
+	
+	printf(" Toggled Character is : %c\n",cRet);
+	
+	
+	return 0;
+}
