13a4888586db1f177a5a59c911e44870f4a760e9
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-06 15:01:08 +0530

    Add files via upload

---
+++ Program267.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Program267
+{
+	public static void main(String a[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Please enter your full name : ");
+		String str = sobj.nextLine();   // string is data type in java
+		
+		System.out.println("Welcome "+str);
+	}
+}
---
+++ Program268.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Program268
+{
+	public static void main(String a[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Please enter your full name : ");
+		String str = sobj.nextLine();   
+		
+		System.out.println("Number of characters are : "+str.length());
+	}
+}
+
+// logic lihana shakya nahi java madhe
